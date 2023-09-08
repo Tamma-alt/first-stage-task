828d8f092187d90c074c6263db8f0ed9085b0809
Tamma-alt <84251673+Tamma-alt@users.noreply.github.com>
2023-09-08 21:00:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// function to get the day of the week
+
+function getCurrentDayOfWeek() {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const currentDay = new Date();
+    const dayOfWeekIndex = currentDay.getDay();
+    return days[dayOfWeekIndex];
+}
+
+const dayOfTheWeekElement = document.getElementById('dayOfWeek');
+const currentDayOfWeek = getCurrentDayOfWeek();
+dayOfTheWeekElement.textContent = `Today is ${currentDayOfWeek}`
+
+// function to get curent UTC Time in milliseconds
+function getCurrentUTCTime () {
+    const today = new Date();
+    const utcTimeInMilliseconds = today.getTime();
+    return utcTimeInMilliseconds;
+}
+
+const utcTime = document.getElementById('utcTime');
+const currentUTCTime = getCurrentUTCTime();
+
+utcTime.textContent = currentUTCTime.toString();
